85d272d5e11e9ff6e499d65163bbe116b3127ee4
Vladislav <cheapsbl@gmail.com>
2018-02-08 14:47:05 +0300

    engine module added

---
+++ engine/src/com/argonaut/BaseActor.java
@@ -0,0 +1,45 @@
+package com.argonaut;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by vladi on 08.02.2018.
+ */
+
+public class BaseActor extends Actor {
+
+    private Texture texture;
+    private TextureRegion region;
+
+    public BaseActor(Texture texture, float x, float y, float width, float height){
+        this.texture = texture;
+        region = new TextureRegion(texture);
+        this.setX(x);
+        this.setY(y);
+        this.setWidth(width);
+        this.setHeight(height);
+        this.setOriginX(0);
+        this.setOriginY(0);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+    public void print(){
+        System.out.println("ITS ME");
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        batch.draw(region, getX(), getY(), getWidth(), getHeight());
+    }
+
+    public void dispose(){
+        texture.dispose();
+    }
+}
