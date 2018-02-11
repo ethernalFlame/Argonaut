3ee05af7360e73a5f55527b9ca51401a57bcc424
Vladislav <cheapsbl@gmail.com>
2018-02-11 12:53:51 +0300

    Camera moving if tile is pressed (camera moves to the tile left bottom corner)

--- desktop/src/com/argonaut/desktop/DesktopLauncher.java
+++ desktop/src/com/argonaut/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.argonaut.MainClass;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//config.width = 1080;
+		//config.height = 1920;
 		new LwjglApplication(new MainClass(), config);
+
 	}
 }
